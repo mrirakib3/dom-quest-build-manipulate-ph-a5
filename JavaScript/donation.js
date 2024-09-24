b6eb5ca0380970e75e5061b6058dd0da16941512
Md Rakibul Islam Rakib <mri.rakib03@gmail.com>
2024-09-25 04:29:13 +0600

    donatin input OK

--- JavaScript/donation.js
+++ JavaScript/donation.js
@@ -1,1 +1,25 @@
-console.log("hello");
+document
+  .getElementById("btn-noakhali")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    //console.log('ok');
+    const noakhaliInput = document.getElementById(
+      "input-noakhali-balance"
+    ).value;
+    //console.log(noakhaliInput);
+    if (noakhaliInput >= 0) {
+      // console.log(typeof noakhaliInput);
+
+      const noakhaliBalance = parseFloat(document.getElementById("noakhali-balance").innerText);
+
+      const noakhaliBalanceNew = noakhaliBalance + parseFloat(noakhaliInput);
+    //   console.log(noakhaliBalanceNew);
+
+      document.getElementById('noakhali-balance').innerText = noakhaliBalanceNew;
+
+    } else {
+      alert("Please, give a valid amount of donation.");
+    }
+  });
+
+//console.log(typeof noakhaliBalance);
