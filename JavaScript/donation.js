3c7222432e582c80c78e834754be8e8cbd034ea9
Md Rakibul Islam Rakib <mri.rakib03@gmail.com>
2024-09-25 11:09:10 +0600

    commit for submit

--- JavaScript/donation.js
+++ JavaScript/donation.js
@@ -1,25 +1,101 @@
+// doanation for noakhali
 document
   .getElementById("btn-noakhali")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    //console.log('ok');
+
     const noakhaliInput = document.getElementById(
       "input-noakhali-balance"
     ).value;
-    //console.log(noakhaliInput);
-    if (noakhaliInput >= 0) {
-      // console.log(typeof noakhaliInput);
 
-      const noakhaliBalance = parseFloat(document.getElementById("noakhali-balance").innerText);
+    if (noakhaliInput >= 0) {
+      const noakhaliBalance = parseFloat(
+        document.getElementById("noakhali-balance").innerText
+      );
 
       const noakhaliBalanceNew = noakhaliBalance + parseFloat(noakhaliInput);
-    //   console.log(noakhaliBalanceNew);
 
-      document.getElementById('noakhali-balance').innerText = noakhaliBalanceNew;
+      document.getElementById("noakhali-balance").innerText =
+        noakhaliBalanceNew;
 
+      const totalBalance = parseFloat(
+        document.getElementById("total-balance").innerText
+      );
+
+      document.getElementById("total-balance").innerText =
+        totalBalance - parseFloat(noakhaliInput);
     } else {
       alert("Please, give a valid amount of donation.");
     }
   });
 
-//console.log(typeof noakhaliBalance);
+
+
+
+// doanation for feni
+document
+  .getElementById("btn-feni")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const feniInput = document.getElementById(
+      "input-feni-balance"
+    ).value;
+
+    if (feniInput >= 0) {
+      const feniBalance = parseFloat(
+        document.getElementById("feni-balance").innerText
+      );
+
+      const feniBalanceNew = feniBalance + parseFloat(feniInput);
+
+      document.getElementById("feni-balance").innerText =
+        feniBalanceNew;
+
+      const totalBalance = parseFloat(
+        document.getElementById("total-balance").innerText
+      );
+
+      document.getElementById("total-balance").innerText =
+        totalBalance - parseFloat(feniInput);
+    } else {
+      alert("Please, give a valid amount of donation.");
+    }
+  });
+
+
+
+
+
+  // doanation for quota
+document
+.getElementById("btn-quota")
+.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const quotaInput = document.getElementById(
+    "input-quota-balance"
+  ).value;
+
+  if (quotaInput >= 0) {
+    const quotaBalance = parseFloat(
+      document.getElementById("quota-balance").innerText
+    );
+
+    const quotaBalanceNew = quotaBalance + parseFloat(quotaInput);
+
+    document.getElementById("quota-balance").innerText =
+      quotaBalanceNew;
+
+    const totalBalance = parseFloat(
+      document.getElementById("total-balance").innerText
+    );
+
+    document.getElementById("total-balance").innerText =
+      totalBalance - parseFloat(quotaInput);
+  } else {
+    alert("Please, give a valid amount of donation.");
+  }
+});
+
+
